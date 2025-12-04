03e6acf26ca0800f916ecbd9215e9ff51fb3b97c
István Hetesi <istvan.hetesi@nevis-security.com>
2025-12-04 14:52:36 +0100

    Added missing keyboard shortcuts to the configuration.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
             vim.keymap.set("n", "<leader>fg", builtin.grep_string, { desc = "Telescope grep text" })
+            vim.keymap.set("v", "_g", builtin.grep_string, { desc = "Telescope grep selection" })
             vim.keymap.set("n", "<leader>fl", builtin.live_grep, { desc = "Telescope live grep" })
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
             vim.keymap.set(
@@ -18,6 +19,10 @@ return {
             vim.keymap.set("n", "<leader>fx", builtin.command_history, { desc = "Telescope command history" })
             vim.keymap.set("n", "<leader>fs", builtin.search_history, { desc = "Telescope search history" })
             vim.keymap.set("n", "<leader>fu", builtin.spell_suggest, { desc = "Telescope spell suggestions" })
+            vim.keymap.set("n", "<leader>s", "/<C-R><C-W><CR>N", { desc = "Search the word under the cursor" })
+            vim.keymap.set("n", "<leader>c", function()
+                vim.fn.setreg("/", "")
+            end, { desc = "Clear search" })
         end,
     },
     {

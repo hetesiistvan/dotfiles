547250859b6ef350b695f6f2e5a68f14615e2b11
István Hetesi <istvan.hetesi@nevis-security.com>
2025-12-09 12:34:39 +0100

    Added two new terminal definitions.

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -35,5 +35,51 @@ return {
             lazygit_open,
             { noremap = true, silent = true, desc = "Toggle LazyGit terminal" }
         )
+
+        local h_terminal = nil
+
+        local function h_terminal_open()
+            if h_terminal == nil then
+                h_terminal = Terminal:new({
+                    direction = "horizontal",
+                    hidden = false,
+                    size = 20,
+                    on_open = function()
+                        vim.cmd("startinsert!")
+                    end,
+                })
+            end
+            h_terminal:toggle()
+        end
+
+        vim.keymap.set(
+            "n",
+            "<leader>th",
+            h_terminal_open,
+            { noremap = true, silent = true, desc = "Toggle horizontal terminal" }
+        )
+
+        local f_terminal = nil
+
+        local function f_terminal_open()
+            if f_terminal == nil then
+                f_terminal = Terminal:new({
+                    direction = "float",
+                    hidden = false,
+                    size = 20,
+                    on_open = function()
+                        vim.cmd("startinsert!")
+                    end,
+                })
+            end
+            f_terminal:toggle()
+        end
+
+        vim.keymap.set(
+            "n",
+            "<leader>tf",
+            f_terminal_open,
+            { noremap = true, silent = true, desc = "Toggle floating terminal" }
+        )
     end,
 }

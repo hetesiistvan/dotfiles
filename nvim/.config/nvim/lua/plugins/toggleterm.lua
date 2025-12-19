fbc727aecc937370e24ed21aafe3f702b3e9c6e1
István Hetesi <istvan.hetesi@nevis-security.com>
2025-12-19 17:32:10 +0100

    Added a shortcut to create a PR.

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -81,5 +81,29 @@ return {
             f_terminal_open,
             { noremap = true, silent = true, desc = "Toggle floating terminal" }
         )
+
+        local pr_terminal = nil
+
+        local function pr_terminal_open()
+            if pr_terminal == nil then
+                pr_terminal = Terminal:new({
+                    cmd = "gh pr create",
+                    direction = "float",
+                    hidden = false,
+                    size = 20,
+                    on_open = function()
+                        vim.cmd("startinsert!")
+                    end,
+                })
+            end
+            pr_terminal:toggle()
+        end
+
+        vim.keymap.set(
+            "n",
+            "<leader>gg",
+            pr_terminal_open,
+            { noremap = true, silent = true, desc = "Toggle Pull Request terminal" }
+        )
     end,
 }

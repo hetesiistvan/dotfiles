f66fcfbda0f4656b4a43d8e684925f06f28e9170
István Hetesi <istvan.hetesi@nevis-security.com>
2025-12-28 13:02:48 +0100

    Added some further exciting settings.

--- nvim/.config/nvim/lua/config/basic-settings.lua
+++ nvim/.config/nvim/lua/config/basic-settings.lua
@@ -3,7 +3,14 @@ local M = {}
 function M.setup()
     -- basic settings
     vim.opt.number = true
+    vim.opt.scrolloff = 8
+    vim.opt.sidescrolloff = 8
     vim.opt.cursorline = true
+    vim.opt.signcolumn = "yes"
+    vim.opt.colorcolumn = "100"
+    vim.opt.wrap = true
+    vim.opt.termguicolors = true
+    vim.opt.showmatch = true
 
     -- Leader key
     vim.g.mapleader = " "
@@ -13,6 +20,12 @@ function M.setup()
     vim.opt.tabstop = 4
     vim.opt.shiftwidth = 4
     vim.opt.expandtab = true
+
+    -- file related settings
+    vim.opt.autoread = true
+    vim.opt.swapfile = false
+    vim.opt.backup = false
+    vim.opt.writebackup = false
 end
 
 return M
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,3 +1,20 @@
+local language_servers = {
+    "lua_ls",
+    "bashls",
+    "awk_ls",
+    "dockerls",
+    "gopls",
+    "jdtls",
+    "jqls",
+    "jsonls",
+    "marksman",
+    "pyright",
+    "sqls",
+    "terraformls",
+    "lemminx",
+    "yamlls",
+}
+
 return {
     {
         "mason-org/mason.nvim",
@@ -16,22 +33,7 @@ return {
         },
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "bashls",
-                    "awk_ls",
-                    "dockerls",
-                    "gopls",
-                    "jdtls",
-                    "jqls",
-                    "jsonls",
-                    "marksman",
-                    "pyright",
-                    "sqls",
-                    "terraformls",
-                    "lemminx",
-                    "yamlls",
-                },
+                ensure_installed = language_servers,
             })
         end,
     },
@@ -43,34 +45,10 @@ return {
         config = function()
             local cmp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            vim.lsp.config("lua_ls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("lua_ls")
-            vim.lsp.config("bashls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("bashls")
-            vim.lsp.config("awk_ls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("awk_ls")
-            vim.lsp.config("dockerls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("dockerls")
-            vim.lsp.config("gopls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("gopls")
-            vim.lsp.config("jdtls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("jdtls")
-            vim.lsp.config("jqls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("jqls")
-            vim.lsp.config("jsonls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("jsonls")
-            vim.lsp.config("marksman", { capabilities = cmp_capabilities })
-            vim.lsp.enable("marksman")
-            vim.lsp.config("pyright", { capabilities = cmp_capabilities })
-            vim.lsp.enable("pyright")
-            vim.lsp.config("sqls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("sqls")
-            vim.lsp.config("terraformls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("terraformls")
-            vim.lsp.config("lemminx", { capabilities = cmp_capabilities })
-            vim.lsp.enable("lemminx")
-            vim.lsp.config("yamlls", { capabilities = cmp_capabilities })
-            vim.lsp.enable("yamlls")
+            for _, language_server in pairs(language_servers) do
+                vim.lsp.config(language_server, { capabilities = cmp_capabilities })
+                vim.lsp.enable(language_server)
+            end
 
             vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "LSP hover" })
             vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "LSP code actions" })
